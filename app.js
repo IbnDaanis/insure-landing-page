684c635815f2ad45eeb7fed5cd4b2789cfc94e5b
Ikraam <IbnDaanis@Gmail.com>
2020-10-16 23:16:23 +0100

    More updates

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ const faBars = document.querySelector('.fa-bars')
 hamburger.addEventListener('click', e => {
   navMobile.classList.toggle('nav-change')
   faBars.classList.toggle('fa-times')
+  document.body.classList.toggle('no-scrolling')
 })
